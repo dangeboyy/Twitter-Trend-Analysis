2f7817a3eedd9f042b022c347b57bb4829e84e2d
İbrahim Ekmen <ibrahimekmentr70@gmail.com>
2021-12-17 19:45:14 +0300

    unused variable deleted

--- tez.py
+++ tez.py
@@ -99,7 +99,6 @@ def add_to_same_array(trend_name, full_array,tweets):
         #     rt_count +=1
 
         full_array.append(text)
-    return rt_count
 
 def text_blob_analysis(currentAnalysis):
     if currentAnalysis.sentiment.polarity == 0:
@@ -177,7 +176,7 @@ def traversingTrends(tweeter_trends):
         #removed_same = set()
         full_array=[]
 
-        rt_count = add_to_same_array(trend_name, full_array, tweets)
+        add_to_same_array(trend_name, full_array, tweets)
         analyzer=SentimentIntensityAnalyzer()
 
         for tweet in full_array:
@@ -220,7 +219,6 @@ def traversingTrends(tweeter_trends):
         print_polarity_result(polarity_tb,polarity_vdr)
         #print("Removed_Same Size:" + str(len(removed_same)))
         print("full size:"+str(len(full_array)))
-        print("retweet size: " + str(rt_count))
         #removed_same.clear()
         full_array.clear()
         break
