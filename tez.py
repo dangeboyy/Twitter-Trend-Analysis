8c1ad56a49b51ace6054c25972c273139b45932f
mert <mertinyo66@gmail.com>
2021-12-18 16:11:41 +0300

    Pie graph and Bar Chart added

--- tez.py
+++ tez.py
@@ -14,7 +14,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 consumer_key = 'z9QiZqqYDtz2iuD6YbnuVo1NS'
 consumer_secret_key = 'hWdiLwJjRhDKRGmqRnrCeRnDifZCRK6l42QplQHx9akGAr2Qk6'
 
@@ -41,8 +40,6 @@ accessSecret = 'XpNAnbkoj6p0EnLuYvqm5vh0AWfJaaIFe80i1QWJ6HMQM'
 authentication = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret_key)
 authentication.set_access_token(accessToken, accessSecret)
 api = tweepy.API(authentication)
-
-
 
 trend_headers = {
     'Authorization': 'Bearer {}'.format(access_token)
@@ -71,6 +68,25 @@ def parse_tweet(search):
     pass
 
 
+def pie_chart(positive, negative, neutral):
+    y = np.array([positive, negative, neutral])
+    mylabels = ["Positives", "Negatives", "Neutrals"]
+    p, tx, autotexts = plt.pie(y, labels=mylabels,
+                               autopct="", shadow=True)
+
+    for i, a in enumerate(autotexts):
+        a.set_text("{}".format(y[i]))
+    plt.axis('equal')
+    plt.show()
+
+
+def bar_chart(positive, negative, neutral):
+    labels = ['positive', 'negative', 'neutral']
+    sentiment = [float(positive), float(negative), float(neutral)]
+    plt.bar(labels, sentiment, color= ['Blue', 'Red', 'green'])
+    plt.show()
+
+
 tweet_id_url = "https://api.twitter.com/2/tweets"
 
 choices = {"İ": "I", "ş": "s", "ı": "i", "Ş": "S", "Ö": "O", "ö": "o", "Ü": "U", "ü": "u", "Ç": "C", "ç": "c",
@@ -83,39 +99,44 @@ writer = csv.writer(f)
 def calculatePercentage(a, b):
     return 100 * float(a) / float(b)
 
-def add_to_same_array(trend_name, full_array,tweets):
+
+def add_to_same_array(trend_name, full_array, tweets):
     rt_count = 0
     for tweet in tweets:
         if "retweeted_status" in dir(tweet):
-            rt_count +=1
+            rt_count += 1
 
         full_array.append(tweet)
 
     return rt_count
 
-def df_for_clustering(pos,neg,pol):
-    Data={'x':pos,'y':neg,'z':pol
-     }
-    df=DataFrame(Data, columns=['x','y','z'])
+
+def df_for_clustering(pos, neg, pol):
+    Data = {'x': pos, 'y': neg, 'z': pol
+            }
+    df = DataFrame(Data, columns=['x', 'y', 'z'])
     return df
 
-def vader_analysis(analyzer,tweet):
-    if (analyzer.polarity_scores(tweet))["compound"]==0:
+
+def vader_analysis(analyzer, tweet):
+    if (analyzer.polarity_scores(tweet))["compound"] == 0:
         print("Neutral for Vader")
-        return 1,0,0
+        return 1, 0, 0
     elif (analyzer.polarity_scores(tweet))["compound"] > 0.00:
         print("Positive for Vader")
-        return 0,1,0
+        return 0, 1, 0
     elif (analyzer.polarity_scores(tweet))["compound"] < 0.00:
         print("Negative for Vader")
-        return 0,0,1
+        return 0, 0, 1
 
-def print_vader_total(positive_vdr,negative_vdr,neutral_vdr):
+
+def print_vader_total(positive_vdr, negative_vdr, neutral_vdr):
     print('-----------------------Vader-----------------------------------------------------')
     print("Pozitif:" + positive_vdr)
     print("Negatif:" + negative_vdr)
     print("Neutral:" + neutral_vdr)
     print("-----------------------------------------------------------------------------------")
+
 
 def print_polarity_result(polarity_vdr):
     print("##########Vader###########")
@@ -127,97 +148,92 @@ def print_polarity_result(polarity_vdr):
         print('Neutral')
     print("#################################")
 
-def traversingTrends(tweeter_trends):
 
+def traversingTrends(tweeter_trends):
     for i in range(len(tweeter_trends['trends'])):
-        trend_name = tweeter_trends['trends'][i+1]['name']
+        trend_name = tweeter_trends['trends'][i + 1]['name']
         print("Trend Name: " + trend_name)
-     
+
         filtered_tweet_array = []
 
         total_positive_vdr = 0
         total_negative_vdr = 0
-        total_neutral_vdr= 0
+        total_neutral_vdr = 0
         total_polarity_vdr = 0
 
-
-        tweets = tweepy.Cursor(api.search_tweets, q=trend_name + " -RT", lang='en',tweet_mode="extended").items(100)
-        full_array=[]
-        positive_values=[]
-        negative_values=[]
-        polarity_values=[]
+        tweets = tweepy.Cursor(api.search_tweets, q=trend_name + " -RT", lang='en', tweet_mode="extended").items(100)
+        full_array = []
+        positive_values = []
+        negative_values = []
+        polarity_values = []
 
         rt_count = add_to_same_array(trend_name, full_array, tweets)
-        analyzer=SentimentIntensityAnalyzer()
+        analyzer = SentimentIntensityAnalyzer()
 
         for tweet in full_array:
-            
-            tweet.full_text = tweet.full_text.replace(trend_name,"")
-            
+            tweet.full_text = tweet.full_text.replace(trend_name, "")
+
             print(tweet.full_text)
 
-            total_polarity_vdr+=(analyzer.polarity_scores(tweet.full_text))["compound"]
-            neut_vdr,pos_vdr,neg_vdr = vader_analysis(analyzer,tweet.full_text)
+            total_polarity_vdr += (analyzer.polarity_scores(tweet.full_text))["compound"]
+            neut_vdr, pos_vdr, neg_vdr = vader_analysis(analyzer, tweet.full_text)
             total_neutral_vdr += neut_vdr
             total_positive_vdr += pos_vdr
             total_negative_vdr += neg_vdr
-            
-            positive_values.append(analyzer.polarity_scores(tweet.full_text)["pos"]+1)
-            negative_values.append(analyzer.polarity_scores(tweet.full_text)["neg"]+1)
-            polarity_values.append(analyzer.polarity_scores(tweet.full_text)["compound"]+1)
 
+            positive_values.append(analyzer.polarity_scores(tweet.full_text)["pos"] + 1)
+            negative_values.append(analyzer.polarity_scores(tweet.full_text)["neg"] + 1)
+            polarity_values.append(analyzer.polarity_scores(tweet.full_text)["compound"] + 1)
 
             tweet_json_object = {
                 "id": tweet.id,
                 "lang": tweet.lang,
-                "retweet_count" : tweet.retweet_count,
-                "favorite_count" : tweet.favorite_count,
-                "full_text" : tweet.full_text
+                "retweet_count": tweet.retweet_count,
+                "favorite_count": tweet.favorite_count,
+                "full_text": tweet.full_text
             }
 
             filtered_tweet_array.append(tweet_json_object)
-            
 
-        #append_single_tweet_to_JSON(filtered_tweet_array, trend_name)
+        # append_single_tweet_to_JSON(filtered_tweet_array, trend_name)
 
-        append_to_JSON_file(filtered_tweet_array,trend_name)
-        
+        append_to_JSON_file(filtered_tweet_array, trend_name)
 
         # numberOfTweets=positive+negative+neutral
         # positive=calculatePercentage(positive,numberOfTweets)
         # negative = calculatePercentage(negative, numberOfTweets)
         # neutral = calculatePercentage(neutral, numberOfTweets)
 
-
         positive_vdr = format(total_positive_vdr, '.2f')
         negative_vdr = format(total_negative_vdr, '.2f')
         neutral_vdr = format(total_neutral_vdr, '.2f')
 
-
-        print_vader_total(positive_vdr,negative_vdr,neutral_vdr)
+        print_vader_total(positive_vdr, negative_vdr, neutral_vdr)
         print_polarity_result(total_polarity_vdr)
         print("full size: " + str(len(full_array)))
         print("retweet size: " + str(rt_count))
 
-        df = df_for_clustering(positive_values,negative_values,polarity_values)
-        print("df")
-        print(df)
-        print("df")
-        # clustering = DBSCAN(eps=5, min_samples=3).fit(df)
-        # labels = clustering.labels_
-        # numberofclusters = len(set(labels)) - (1 if -1 in labels else 0)
-        # plt.title('DBSCAN ' + labels + ' Number of clusters: %d' % numberofclusters)
-        # plt.scatter(df['X'], df['Y'],df['Z'], c=clustering.labels_.astype(float))
+        # df = df_for_clustering(positive_values, negative_values, polarity_values)
+        # print("df")
+        # print(df)
+        # print("df")
+        # # clustering = DBSCAN(eps=5, min_samples=3).fit(df)
+        # # labels = clustering.labels_
+        # # numberofclusters = len(set(labels)) - (1 if -1 in labels else 0)
+        # # plt.title('DBSCAN ' + labels + ' Number of clusters: %d' % numberofclusters)
+        # # plt.scatter(df['X'], df['Y'],df['Z'], c=clustering.labels_.astype(float))
+        # # plt.show()
+        # kmeans = KMeans(n_clusters=3, init='k-means++').fit(df)
+        # centroids = kmeans.cluster_centers_
+        # plt.scatter(df['x'], df['y'], df['z'], c=kmeans.labels_.astype(float))
+        # plt.scatter(centroids[:, 0], centroids[:, 1], c='red')
+        # plt.xlabel("positives")
+        # plt.ylabel("negatives")
         # plt.show()
-        kmeans=KMeans(n_clusters=3 , init='k-means++').fit(df)
-        centroids = kmeans.cluster_centers_
-        plt.scatter(df['x'],df['y'],df['z'],c=kmeans.labels_.astype(float))
-        plt.scatter(centroids[:, 0], centroids[:, 1], c='red')
-        plt.xlabel("positives")
-        plt.ylabel("negatives")
-        plt.show()
 
         full_array.clear()
+        pie_chart(positive_vdr, negative_vdr, neutral_vdr)
+        bar_chart(positive_vdr, negative_vdr, neutral_vdr)
         break
 
     f.close()
